9cbea1a232425a0a14befc0511ff5c64b089753a
lahariSane <135339666+lahariSane@users.noreply.github.com>
2024-08-10 09:46:11 +0530

    coding Playground

--- frontend/src/components/codingPlayground/OutputWindow.js
+++ frontend/src/components/codingPlayground/OutputWindow.js
@@ -35,10 +35,10 @@ const OutputWindow = ({ outputDetails }) => {
   };
   return (
     <>
-      <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
+      <h1 className="font-bold text-xl bg-clip-text text-white bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
         Output
       </h1>
-      <div className="w-full h-56 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
+      <div className="w-full h-56 bg-[#1e293b] rounded-md text-white font-normal text-md overflow-y-auto text-white p-3">
         {outputDetails ? <>{getOutput()}</> : null}
       </div>
     </>
